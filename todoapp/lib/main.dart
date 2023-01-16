210f89db494d403295550ff878ef789e65f742a2
Elton Fungirai <eltonfungirai049@gmail.com>
2023-01-16 16:12:55 +0200

    commit

---
+++ todoapp/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:todoapp/models/task.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Task> tasks = [
+    Task(
+        title: 'Task 1',
+        description: 'This is the first task',
+        dueDate: DateTime.now()),
+    Task(
+        title: 'Task 2',
+        description: 'This is the second task',
+        dueDate: DateTime.now().add(Duration(days: 1))),
+    Task(
+        title: 'Task 3',
+        description: 'This is the third task',
+        dueDate: DateTime.now().add(Duration(days: 2))),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo App'),
+      ),
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(tasks[index].title),
+            subtitle: Text(tasks[index].description),
+            trailing: Text(tasks[index].dueDate.toString()),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigate to the add task screen
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ todoapp/lib/models/task.dart
@@ -0,0 +1,13 @@
+class Task {
+  final String title;
+  final String description;
+  final DateTime dueDate;
+  bool isCompleted;
+
+  Task({
+    required this.title,
+    required this.description,
+    required this.dueDate,
+    this.isCompleted = false,
+  });
+}
